e8d9e5f3e785e824bc9751bfe0632f25514457e0
rafi <rafiahmedrabby282@gmail.com>
2025-07-05 22:21:22 +0600

    added cors origin

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,9 @@ var cors = require('cors')
 const express = require("express");
 const app: Application = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: ['https://library-management-client-indol.vercel.app']
+   }))
 app.use("/api/books", booksRoutes);
 app.use("/api/borrow", borrowBooksRoutes);
 app.get("/", (req: Request, res: Response) => {
